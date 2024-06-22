91b71c987e4a28b099265675606c9461abed4a05
Kirill Bordiyan <kirillbordiyan@gmail.com>
2024-06-22 15:52:58 +0300

    if book take authors (many to one)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,70 @@
 package org.example;
 
+import org.example.entity.Author;
+import org.example.entity.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory()) {
+//            withCRUD(sessionFactory);
+            sessinFactory(sessionFactory);
+        }
+    }
+
+    private static void sessinFactory(SessionFactory sessionFactory){
+        try (Session session = sessionFactory.openSession()) {
+            Book book = session.find(Book.class, 1L);
+            System.out.println(book);
+            System.out.println(book.getAuthor());
+        }
+
+
+    }
+
+
+    private static void withCRUD(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.openSession()) {
+
+            Author author = session.find(Author.class, 1L); // select
+            System.out.println("1: " + author);
+        }
+
+        try (Session session = sessionFactory.openSession()) {
+            Transaction t = session.beginTransaction();
+
+            Author author = new Author();
+            author.setId(2L);
+            author.setName("Auth2");
+
+            session.persist(author); // insert
+            t.commit();
+        }
+
+
+        try (Session session = sessionFactory.openSession()) {
+
+            Author author = session.find(Author.class, 2L);
+            author.setName("UPDATED");
+
+            Transaction t = session.beginTransaction();
+            session.merge(author); // update
+            t.commit();
+
+        }
+
+        try (Session session = sessionFactory.openSession()) {
+            Author author = session.find(Author.class, 1L);
+
+            Transaction t = session.beginTransaction();
+            session.remove(author); // delete
+            t.commit();
+
+        }
     }
 }
--- src/main/java/org/example/entity/Book.java
+++ src/main/java/org/example/entity/Book.java
@@ -1,24 +1,26 @@
 package org.example.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "author")
+@Table(name = "book")
 @Getter
 @Setter
 @ToString
-public class Author {
+public class Book {
+
     @Id
     @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
     private String name;
-}
 
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+}
